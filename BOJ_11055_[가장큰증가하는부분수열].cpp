29c719c3246b05e267ae06a39469fca4dbee563b
Double_J <double04j@gmail.com>
2023-06-17 13:59:17 +0900

    BOJ_11055_[가장큰증가하는부분수열]_DP

--- BOJ_11055_[가장큰증가하는부분수열].cpp
+++ BOJ_11055_[가장큰증가하는부분수열].cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
 
 using namespace std;
 
@@ -12,7 +13,23 @@ int main(void){
 
     for(int i=0;i<num;i++){
         cin>>vc[i];
+        dp[i]=vc[i];
+        //자기자신이 가장 큰 경우일 수도 있음으로 이렇게 초기화.
     }
+    for(int i=0;i<num;i++){
+        for(int j=0;j<i;j++){
+            if(j<i && vc[j]<vc[i]){
+                dp[i] = max(dp[i], dp[j]+vc[i]);
+            }
+        }
+    }
+    /*
+    for(int i=0;i<num;i++){
+        cout<<dp[i]<<" ";
+    }*/
+    sort(dp.begin(), dp.end(), greater<int>());
+
+    cout<<dp[0];
 
 
     return 0;
